9883f166d4ce47a79bdc58b9b83b13ed5a81f17f
niuhaijun <niuhaijun@100tal.com>
2018-08-07 09:40:51 +0800

    调整代码

--- src/main/java/com/smart/i18n/LocaleSample.java
+++ src/main/java/com/smart/i18n/LocaleSample.java
@@ -33,16 +33,12 @@ public class LocaleSample {
      * */
     String pattern1 = "{0}，你好！你于{1}在工商银行存入{2} 元。";
     String pattern2 = "At {1,time,short} On{1,date,long}，{0} paid {2,number,currency}.";
-
     // 用于动态替换占位符的参数
     Object[] params = {"John", new GregorianCalendar().getTime(), 1.0E3};
-
     String msg1 = MessageFormat.format(pattern1, params);
     System.out.println(msg1);
-
     MessageFormat mf = new MessageFormat(pattern2, Locale.US);
     String msg2 = mf.format(params);
-
     System.out.println(msg2);
   }
 
