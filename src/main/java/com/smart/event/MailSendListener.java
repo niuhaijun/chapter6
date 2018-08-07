cd4ccd15b2a7a3ee1027fe2509947223b9206811
niuhaijun <niuhaijun@100tal.com>
2018-08-07 15:09:33 +0800

    添加注释

--- src/main/java/com/smart/event/MailSendListener.java
+++ src/main/java/com/smart/event/MailSendListener.java
@@ -7,9 +7,13 @@ import org.springframework.context.ApplicationListener;
  */
 public class MailSendListener implements ApplicationListener<MailSendEvent> {
 
+  /**
+   * 对MailSendEvent事件进行处理
+   *
+   * @param event 事件
+   */
   @Override
   public void onApplicationEvent(MailSendEvent event) {
-    MailSendEvent mse = (MailSendEvent) event;
-    System.out.println("MailSendListener:向" + mse.getTo() + "发送完一封邮件");
+    System.out.println("MailSendListener:向" + event.getTo() + "发送完一封邮件");
   }
 }
